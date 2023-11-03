0fd2fe0a2a409b8dabea8a4d865bc69d926d7d1a
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-03 08:06:31 +0100

    Add HashMap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String dog = "wuffi";
+        String cat = "mutzi";
+        String bird = "tweety";
+
+        Set<String> animalsFed = new HashSet<>();
+
+        animalsFed.add(dog);
+        animalsFed.add(cat);
+        animalsFed.add(bird);
+        animalsFed.add(bird);
+        animalsFed.add(cat);
+        animalsFed.add(cat);
+        animalsFed.add(cat);
+
+        for (String animal : animalsFed) {
+            System.out.println("Ich habe heute gefüttert: "+animal);
+        }
     }
 }
